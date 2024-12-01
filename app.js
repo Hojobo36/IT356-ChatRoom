276b374bd63ff09406064cc0050df14243cd95d9
Hojobo36 <jditt2000@gmail.com>
2024-11-30 19:11:45 -0500

    Add WebSocket support to broadcast new chat messages
    Implement WebSocket connections using Socket.IO to enable real-time message updates in the chat application. This update allows new messages to be instantly broadcasted to all connected users without the need for page refreshes. Additionally, the server setup has been modified to support WebSocket functionality.

--- app.js
+++ app.js
@@ -1,54 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const path = require('path');
+const http = require('http'); // Required to create a server for Socket.IO
+const socketIo = require('socket.io');
 
 const app = express();
+const server = http.createServer(app); // Create an HTTP server
+const io = socketIo(server); // Attach Socket.IO to the server
+
+const messagesFile = './messages.json';
+
+const loadMessages = () => {
+    try {
+        return JSON.parse(fs.readFileSync(messagesFile, 'utf8'));
+    } catch (err) {
+        return [];
+    }
+};
+
+const saveMessages = (messages) => {
+    fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
+};
+
+let users = loadMessages();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Path to the messages JSON file
-const messagesFilePath = path.join(__dirname, 'messages.json');
-
-// Function to read the messages from the JSON file
-function getMessages() {
-    if (fs.existsSync(messagesFilePath)) {
-        const data = fs.readFileSync(messagesFilePath, 'utf8');
-        return JSON.parse(data);
-    }
-    return []; // Return an empty array if no file exists
-}
-
-// Function to save the messages to the JSON file
-function saveMessages(messages) {
-    fs.writeFileSync(messagesFilePath, JSON.stringify(messages, null, 2));
-}
-
-// Render home page with stored messages
+// Serve the home page
 app.get("/", function (req, res) {
-    const users = getMessages();
     res.render("home", { data: users });
 });
 
-// Handle new message submissions
+// Handle new messages
 app.post("/", (req, res) => {
     const inputUserName = req.body.userName;
     const inputUserMessage = req.body.userMessage;
 
-    const users = getMessages();
-    users.push({
-        userName: inputUserName,
-        userMessage: inputUserMessage,
-    });
-
+    const newMessage = { userName: inputUserName, userMessage: inputUserMessage };
+    users.push(newMessage);
     saveMessages(users);
 
-    // Redirect to avoid resubmission of the form on refresh
+    // Emit the new message to all connected clients
+    io.emit('newMessage', newMessage);
+
     res.redirect("/");
 });
 
-app.listen(3000, (req, res) => {
+// Handle WebSocket connections
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
     console.log("App is running on port 3000");
 });
